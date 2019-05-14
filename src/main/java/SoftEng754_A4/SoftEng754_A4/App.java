8ab84323e47625b000345e965711f1ce9d423972
ranveersinghaiden <raid127@aucklanduni.ac.nz>
2019-05-14 12:29:12 +1200

    adding basic file structure.

---
+++ src/main/java/SoftEng754_A4/SoftEng754_A4/App.java
@@ -0,0 +1,13 @@
+package SoftEng754_A4.SoftEng754_A4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
