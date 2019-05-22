73be97088ef4934585b47152d35b7870f0f95ae2
Neha Sarwate <nsar492@aucklanduni.ac.nz>
2019-05-22 13:19:13 +1200

    RED -> Adding Pull Request interface with getPullRequest method signature

---
+++ src/main/java/SoftEng754_A4/SoftEng754_A4/NonDeveloperCodeReview/PullRequest.java
@@ -0,0 +1,6 @@
+package SoftEng754_A4.SoftEng754_A4.NonDeveloperCodeReview;
+
+public interface PullRequest {
+
+	public int getPullRequest(String branch, String createdBy, String linkedWorkItem);
+}
