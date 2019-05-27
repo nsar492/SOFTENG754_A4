478631b132e15d984f5cdcaa4ac413fb9b1818d0
Neha Sarwate <nsar492@aucklanduni.ac.nz>
2019-05-27 13:06:26 +1200

    RED -> Adding the method in the inteface for receiving the Automated code review and abstraction file

--- src/main/java/SoftEng754_A4/SoftEng754_A4/NonDeveloperCodeReview/NonDeveloperCodeReview.java
+++ src/main/java/SoftEng754_A4/SoftEng754_A4/NonDeveloperCodeReview/NonDeveloperCodeReview.java
@@ -4,4 +4,8 @@ import java.io.File;
 
 public interface NonDeveloperCodeReview {
 	public File getAutomatedCodeReviewAndAbstractionFile(String branch, int pullRequestID, String linkedWorkItem);
+	
+	public Boolean establishConnection(String sourceAddress, String destinationAddress);
+	
+	public File receiveAutomatedCodeReviewAndAbstraction(String branch, int pullRequestID, String linkedWorkItem);
 }
