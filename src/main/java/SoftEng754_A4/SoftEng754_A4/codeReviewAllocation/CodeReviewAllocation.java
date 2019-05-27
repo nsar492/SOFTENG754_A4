71d32d31c0e8b3a8f325e6420de047f447f16712
ranveersinghaiden <raid127@aucklanduni.ac.nz>
2019-05-27 13:00:38 +1200

    RED - GREEN - REFACTOR -> Adding test case to validate adding reviewer to database

---
+++ src/main/java/SoftEng754_A4/SoftEng754_A4/codeReviewAllocation/CodeReviewAllocation.java
@@ -0,0 +1,7 @@
+package SoftEng754_A4.SoftEng754_A4.codeReviewAllocation;
+
+public interface CodeReviewAllocation {
+
+	public boolean addReviewerToDatabase();
+
+}
---
+++ src/test/java/SoftEng754_A4/SoftEng754_A4/codeReviewAllocation/TestCodeReviewAllocation.java
@@ -0,0 +1,41 @@
+package SoftEng754_A4.SoftEng754_A4.codeReviewAllocation;
+
+import org.mockito.Mockito;
+
+import junit.framework.TestCase;
+
+public class TestCodeReviewAllocation extends TestCase{
+
+	/*
+	 * The  developer  can  add/delete  one  or  more  non-developer  
+	 * reviewers  in  this  tool.  A  database is used to store the reviewers’ information.
+	 * 9)  The tool can randomly choose a reviewer and allocate code review task 
+	 * to this reviewer. The  chance  of  being  allocated  is  related  to  the  
+	 * count  of  reviews  previously  done  by  a  reviewer, the lower count, the higher chance.  
+	 * 10) The  count  will  be  updated  after  the  allocation.  The  count  information  
+	 * is  stored  in  the  database as well.
+	 * 
+	 * */
+	
+	/*
+	 *  test add non-developer reviewer to database
+	 *  test add non-developer reviewer to database - negative
+	 *  test delete non-developer reviewer to database
+	 *  test delete non-developer reviewer to database - negative
+	 *  test update database
+	 *  test update database - negative
+	 *  test get reviewer list from database
+	 *  test get random reviewer
+	 *  test update reviewer information in database
+	 * */
+	
+	public void testAddReviewerToDatabase()
+	{
+		CodeReviewAllocation reviewAllocator = Mockito.mock(CodeReviewAllocation.class);
+		
+		// When - Then
+		Mockito.when(reviewAllocator.addReviewerToDatabase()).thenReturn(true);
+		
+		assertTrue(reviewAllocator.addReviewerToDatabase());
+	}
+}
